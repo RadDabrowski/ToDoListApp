6386700ca4135b9cfdf4223a93f7bbf23dfe06e6
Radosław Dąbrowski <radoslaw.dab.dabrowski@gmail.com>
2023-05-21 11:57:27 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Radosław Dąbrowski on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
