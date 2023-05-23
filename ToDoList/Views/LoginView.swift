89a82f08b25ad50cc0e0d0c734b44b8dc3dbd75a
Radosław Dąbrowski <radoslaw.dab.dabrowski@gmail.com>
2023-05-23 18:45:50 +0200

    first steps with Login View View Model

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 
 struct LoginView: View {
-    
-    @State var email = ""
-    @State var password = ""
+    @StateObject var viewModel = LoginViewViewModel()
+
 
      var body: some View {
         NavigationView{
@@ -23,22 +22,18 @@ struct LoginView: View {
                            background: Color.pink)
                 // Login Form
                 Form{
-                    TextField("Email Address", text:$email)
+                    TextField("Email Address", text:$viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
-                    TextField("Password", text:$password)
+                        .autocapitalization(.none)
+                    
+                    
+                    SecureField("Password", text:$viewModel.password)
                         .textFieldStyle(DefaultTextFieldStyle())
                     
-                    Button {
-                        
-                    } label: {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(Color.blue)
-                            
-                            Text("Log in")
-                                .foregroundColor(Color.white)
-                                .bold()
-                        }
+                    TLButton(title: "Log in",
+                             background: .blue
+                    ) {
+                        // Attempt log in
                     }
                     .padding()
                 }
